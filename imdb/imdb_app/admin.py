e101978b733ffaa806e7adf667d8fe4145c9b317
Alkesh Kothar <77409891+AlkeshKothar@users.noreply.github.com>
2021-10-02 00:23:41 +0530

    completed

--- imdb/imdb_app/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from imdb_app.models import StreamPlatfrom, WatchList, Review
-# Register your models here.
-
-admin.site.register(StreamPlatfrom)
-admin.site.register(WatchList)
-admin.site.register(Review)
--- imdb/imdb_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ImdbAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'imdb_app'
